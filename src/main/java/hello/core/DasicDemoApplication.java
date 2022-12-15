7c8ed1757b8678078ca93f30613f742259792350
SiHyun-Lee113 <tlgus0113@gmail.com>
2022-12-15 20:43:53 +0900

    create git repository

---
+++ src/main/java/hello/core/DasicDemoApplication.java
@@ -0,0 +1,12 @@
+package hello.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DasicDemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DasicDemoApplication.class, args);
+    }
+
+}
